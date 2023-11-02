e69fbb493fcad85294eaa68a10e105ffb510798d
gokuljs <gokuljs029@gmail.com>
2023-11-03 02:41:52 +0530

    hero-sextion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import styles from './page.module.scss';
+import Hero from './ui/home/Hero/hero';
 export default function Home() {
   return (
-    <div
-      className={styles.homePage}
-      style={{
-        border: '1px solid white',
-      }}
-    >
-      Gokuljs
+    <div className={styles.homePage}>
+      <Hero />
     </div>
   );
 }
--- src/app/ui/home/Hero/hero.tsx
+++ src/app/ui/home/Hero/hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './hero.scss';
 
 const Hero: React.FC = () => {
-  return <div className="hero"></div>;
+  return <div className="hero">dsdsd</div>;
 };
 
 export default Hero;

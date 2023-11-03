a58f85f9f98a5bff2fa510d42388951fc9a2c44e
gokuljs <gokuljs029@gmail.com>
2023-11-03 07:44:01 +0530

    hero section

--- src/app/ui/home/Hero/hero.tsx
+++ src/app/ui/home/Hero/hero.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 import styles from '@styles/hero.module.scss';
 
 const Hero: React.FC = () => {
-  return <div className={styles.hero}>dsdsd</div>;
+  return (
+    <div className={styles.hero}>
+      <div className={styles.content}>
+        <div className={styles.info}>
+          <h1>Gokul JS</h1>
+          <p>Former Founding Engineer at AeroTime, YC W21</p>
+          <p>
+            Passionate about web development and in search of an exciting new
+            gig.
+          </p>
+        </div>
+      </div>
+      <div className={styles.profile}>sasas</div>
+    </div>
+  );
 };
 
 export default Hero;

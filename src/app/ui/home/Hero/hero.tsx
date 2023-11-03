0af071774ddda54c8507c58997ed06754660f774
gokuljs <gokuljs029@gmail.com>
2023-11-03 08:40:55 +0530

    hero-section

--- src/app/ui/home/Hero/hero.tsx
+++ src/app/ui/home/Hero/hero.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import styles from '@styles/hero.module.scss';
+import Image from 'next/image';
 
 const Hero: React.FC = () => {
   return (
     <div className={styles.hero}>
-      <div className={styles.content}>
-        <div className={styles.info}>
-          <h1 className={styles.heading}>
-            Developer. Innovator. Web Enthusiast.
-          </h1>
-          <p className={styles.subtext}>
-            Hey, I'm Gokul JS. Former Founding Engineer at AeroTime, YC W21.
-            Passionate about shaping the web's future and currently looking for
-            a new gig.
-          </p>
-        </div>
-      </div>
-      <div className={styles.profile}></div>
+      <Image
+        className={styles.profile}
+        src="/portfolio.jpeg"
+        width={300}
+        height={300}
+        alt="Gokul JS"
+      />
+      <h1 className={styles.heading}>Developer. Innovator. Web Enthusiast</h1>
+      <p className={styles.subtext}>
+        Hey, I'm Gokul JS. Former Founding Engineer at AeroTime, YC W21.
+        <br />
+        Passionate about shaping the web's future and currently looking for a
+        new gig.
+      </p>
     </div>
   );
 };

7279f5dbfd2f1853be845a672a10b03db310e128
gokuljs <gokuljs029@gmail.com>
2023-11-03 22:59:57 +0530

    github-graph

---
+++ src/app/ui/home/GithubGraph/GithubGraph.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '@styles/githubGraph.module.scss';
+
+const GithubGraph = () => {
+  return <div className={styles.github}>github</div>;
+};
+
+export default GithubGraph;

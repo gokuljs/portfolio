8f72d046626dfe0cb6b8548bce66dcfbc5bc2791
gokuljs <gokuljs029@gmail.com>
2023-11-04 01:25:08 +0530

    github calendar

--- src/app/ui/home/GithubGraph/GithubGraph.tsx
+++ src/app/ui/home/GithubGraph/GithubGraph.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import styles from '@styles/githubGraph.module.scss';
+import GitHubCalendar from 'react-github-calendar';
+import { ScrollArea } from '@radix-ui/themes';
 
 const GithubGraph = () => {
-  return <div className={styles.github}>github</div>;
+  const [loading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const timeOutFn = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+    return () => {
+      clearTimeout(timeOutFn);
+    };
+  }, []);
+
+  return (
+    <div className={styles.github}>
+      <h1></h1>
+      <div className={styles.container}>
+        <GitHubCalendar
+          username="gokuljs"
+          colorScheme="dark"
+          blockSize={15}
+          fontSize={12}
+          loading={loading}
+          style={{
+            color: '#47536B',
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default GithubGraph;

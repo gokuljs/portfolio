d79db116f67cf6d2b4894699d2756ba9831fe6fc
gokuljs <gokuljs029@gmail.com>
2023-11-04 19:11:05 +0530

    skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import styles from '@styles/page.module.scss';
 import Hero from './ui/home/Hero/hero';
 import GithubGraph from './ui/home/GithubGraph/GithubGraph';
+import Skill from './ui/home/Skills/Skill';
 
 export default function Home() {
   return (
     <div className={styles.homePage}>
       <Hero />
       <GithubGraph />
+      <Skill />
     </div>
   );
 }
---
+++ src/app/ui/home/Skills/Skill.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import styles from '@styles/Skills.module.scss';
+const Skill: React.FC = () => {
+  const skills = [
+    {
+      topic: 'Programming Languages',
+      contents: [
+        { name: 'JavaScript', proficiency: 'Advanced' },
+        { name: 'TypeScript', proficiency: 'Intermediate' },
+        { name: 'Python', proficiency: 'Intermediate' },
+        { name: 'HTML', proficiency: 'Advanced' },
+        { name: 'CSS', proficiency: 'Advanced' },
+        { name: 'Sass', proficiency: 'Intermediate' },
+      ],
+    },
+    {
+      topic: 'Frontend Development',
+      contents: [
+        { name: 'React JS', proficiency: 'Advanced' },
+        { name: 'Next JS', proficiency: 'Intermediate' },
+        { name: 'Vite', proficiency: 'Basic' },
+        { name: 'Gatsby', proficiency: 'Basic' },
+      ],
+    },
+    {
+      topic: 'Backend Development',
+      contents: [
+        { name: 'Node.js', proficiency: 'Advanced' },
+        { name: 'Express.js', proficiency: 'Advanced' },
+      ],
+    },
+    {
+      topic: 'Machine Learning and AI Fine-Tuning',
+      contents: [
+        { name: 'LLaMA', proficiency: 'Basic' },
+        { name: 'Stable Diffusion', proficiency: 'Basic' },
+      ],
+    },
+    {
+      topic: 'Cloud Services (AWS)',
+      contents: [
+        { name: 'Amazon S3', proficiency: 'Advanced' },
+        { name: 'Amazon EFS', proficiency: 'Intermediate' },
+        { name: 'Amazon SageMaker', proficiency: 'Basic' },
+        { name: 'AWS Lambda', proficiency: 'Intermediate' },
+        { name: 'AWS Amplify', proficiency: 'Basic' },
+      ],
+    },
+    {
+      topic: 'Content Management Systems',
+      contents: [
+        { name: 'Strapi', proficiency: 'Intermediate' },
+        { name: 'Sanity', proficiency: 'Intermediate' },
+      ],
+    },
+    {
+      topic: 'Version Control Systems',
+      contents: [
+        { name: 'GitHub', proficiency: 'Advanced' },
+        { name: 'GitLab', proficiency: 'Intermediate' },
+      ],
+    },
+  ];
+
+  return (
+    <div className={styles.skills}>
+      <h1 className={styles.heading}>What I know</h1>
+      <div className={styles.container}></div>
+    </div>
+  );
+};
+
+export default Skill;

e20a7cc4d10c74a24d8aa3441f59ef8029d9af4d
gokuljs <gokuljs029@gmail.com>
2023-11-08 22:01:51 +0530

    skills

--- src/app/ui/home/Skills/Skill.tsx
+++ src/app/ui/home/Skills/Skill.tsx
@@ -2,63 +2,47 @@ import React from 'react';
 import styles from '@styles/Skills.module.scss';
 const Skill: React.FC = () => {
   const skills = [
+    { category: 'languages', skills: ['Javascript', 'Typescript', 'Python'] },
     {
-      topic: 'Programming Languages',
-      contents: [
-        { name: 'JavaScript', proficiency: 'Advanced' },
-        { name: 'TypeScript', proficiency: 'Intermediate' },
-        { name: 'Python', proficiency: 'Intermediate' },
-        { name: 'HTML', proficiency: 'Advanced' },
-        { name: 'CSS', proficiency: 'Advanced' },
-        { name: 'Sass', proficiency: 'Intermediate' },
+      category: 'frontend',
+      skills: [
+        'Html',
+        'Vite',
+        'Css',
+        'Sass',
+        'React JS',
+        'Next JS 14',
+        'Gatsby Js',
+        'Redux',
       ],
     },
     {
-      topic: 'Frontend Development',
-      contents: [
-        { name: 'React JS', proficiency: 'Advanced' },
-        { name: 'Next JS', proficiency: 'Intermediate' },
-        { name: 'Vite', proficiency: 'Basic' },
-        { name: 'Gatsby', proficiency: 'Basic' },
-        { name: 'Redux', proficiency: 'Advanced' },
-      ],
+      category: 'backend',
+      skills: ['Node.js', 'Express', 'Aws s3', 'Aws sagemaker', 'Aws efs'],
     },
     {
-      topic: 'Backend Development',
-      contents: [
-        { name: 'Node.js', proficiency: 'Advanced' },
-        { name: 'Express.js', proficiency: 'Advanced' },
+      category: 'LLM',
+      skills: [
+        'GPT-3.5',
+        'GPT-4',
+        'Llama',
+        'Stable diffusion',
+        'Prompt engineering',
       ],
     },
+    { category: 'Version control system', skills: ['git', 'github', 'gitlab'] },
+    { category: 'cms', skills: ['strapi', 'sanity'] },
+
     {
-      topic: 'Machine Learning and AI Fine-Tuning',
-      contents: [
-        { name: 'LLaMA', proficiency: 'Basic' },
-        { name: 'Stable Diffusion', proficiency: 'Basic' },
-      ],
-    },
-    {
-      topic: 'Cloud Services (AWS)',
-      contents: [
-        { name: 'Amazon S3', proficiency: 'Advanced' },
-        { name: 'Amazon EFS', proficiency: 'Intermediate' },
-        { name: 'Amazon SageMaker', proficiency: 'Basic' },
-        { name: 'AWS Lambda', proficiency: 'Intermediate' },
-        { name: 'AWS Amplify', proficiency: 'Basic' },
-      ],
-    },
-    {
-      topic: 'Content Management Systems',
-      contents: [
-        { name: 'Strapi', proficiency: 'Intermediate' },
-        { name: 'Sanity', proficiency: 'Intermediate' },
-      ],
-    },
-    {
-      topic: 'Version Control Systems',
-      contents: [
-        { name: 'GitHub', proficiency: 'Advanced' },
-        { name: 'GitLab', proficiency: 'Intermediate' },
+      category: 'Currently exploring',
+      skills: [
+        'micro services',
+        'Kubernetes',
+        'Docker',
+        'Video based llm',
+        'mistral 7b',
+        'mistral',
+        'Whisper',
       ],
     },
   ];
@@ -66,7 +50,18 @@ const Skill: React.FC = () => {
   return (
     <div className={styles.skills}>
       <h1 className={styles.heading}>What I know</h1>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        {skills.map((item) => (
+          <div className={styles.card}>
+            <div className={styles.header}>{item.category}</div>
+            <div className={styles.cards}>
+              {item.skills.map((item) => (
+                <div className={styles.box}>{item}</div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

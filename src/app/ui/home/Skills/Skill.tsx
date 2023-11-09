b62cda7909878c7a1462cbb17250680db1ad177b
gokuljs <gokuljs029@gmail.com>
2023-11-09 12:59:21 +0530

    padding fix

--- src/app/ui/home/Skills/Skill.tsx
+++ src/app/ui/home/Skills/Skill.tsx
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import styles from '@styles/Skills.module.scss';
 import { skillsWithRatings } from './constant';
-import * as Slider from '@radix-ui/react-slider';
 const Skill: React.FC = () => {
   const [skills, setSkills] = useState(skillsWithRatings);
   console.log(skills);
@@ -13,17 +12,6 @@ const Skill: React.FC = () => {
         {skills.map((item) => (
           <div key={item.name} className={styles.card}>
             <div className={styles.header}>{item.name.toLowerCase()}</div>
-            <Slider.Root
-              className={styles.SliderRoot}
-              defaultValue={[item.rating]}
-              max={10}
-              step={1}
-              disabled
-            >
-              <Slider.Track className={styles.SliderTrack}>
-                <Slider.Range className={styles.SliderRange} />
-              </Slider.Track>
-            </Slider.Root>
           </div>
         ))}
       </div>

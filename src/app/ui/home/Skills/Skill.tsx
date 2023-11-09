abfe900c238ca9fd9589c46289b770ae4ae2a6d4
gokuljs <gokuljs029@gmail.com>
2023-11-09 06:49:47 +0530

    skills

--- src/app/ui/home/Skills/Skill.tsx
+++ src/app/ui/home/Skills/Skill.tsx
@@ -1,64 +1,29 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import styles from '@styles/Skills.module.scss';
+import { skillsWithRatings } from './constant';
+import * as Slider from '@radix-ui/react-slider';
 const Skill: React.FC = () => {
-  const skills = [
-    { category: 'languages', skills: ['Javascript', 'Typescript', 'Python'] },
-    {
-      category: 'frontend',
-      skills: [
-        'Html',
-        'Vite',
-        'Css',
-        'Sass',
-        'React JS',
-        'Next JS 14',
-        'Gatsby Js',
-        'Redux',
-      ],
-    },
-    {
-      category: 'backend',
-      skills: ['Node.js', 'Express', 'Aws s3', 'Aws sagemaker', 'Aws efs'],
-    },
-    {
-      category: 'LLM',
-      skills: [
-        'GPT-3.5',
-        'GPT-4',
-        'Llama',
-        'Stable diffusion',
-        'Prompt engineering',
-      ],
-    },
-    { category: 'Version control system', skills: ['git', 'github', 'gitlab'] },
-    { category: 'cms', skills: ['strapi', 'sanity'] },
-
-    {
-      category: 'Currently exploring',
-      skills: [
-        'micro services',
-        'Kubernetes',
-        'Docker',
-        'Video based llm',
-        'mistral 7b',
-        'mistral',
-        'Whisper',
-      ],
-    },
-  ];
-
+  const [skills, setSkills] = useState(skillsWithRatings);
+  console.log(skills);
   return (
     <div className={styles.skills}>
       <h1 className={styles.heading}>What I know</h1>
       <div className={styles.container}>
         {skills.map((item) => (
-          <div className={styles.card}>
-            <div className={styles.header}>{item.category}</div>
-            <div className={styles.cards}>
-              {item.skills.map((item) => (
-                <div className={styles.box}>{item}</div>
-              ))}
-            </div>
+          <div key={item.name} className={styles.card}>
+            <div className={styles.header}>{item.name.toLowerCase()}</div>
+            <Slider.Root
+              className={styles.SliderRoot}
+              defaultValue={[item.rating]}
+              max={10}
+              step={1}
+              disabled
+            >
+              <Slider.Track className={styles.SliderTrack}>
+                <Slider.Range className={styles.SliderRange} />
+              </Slider.Track>
+            </Slider.Root>
           </div>
         ))}
       </div>

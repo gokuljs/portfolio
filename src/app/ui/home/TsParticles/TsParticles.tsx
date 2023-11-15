b6af7431f010e5b785486d880ecdb6030c9e7a82
gokuljs <gokuljs029@gmail.com>
2023-11-15 17:36:57 +0530

    tsparticles

--- src/app/ui/home/TsParticles/TsParticles.tsx
+++ src/app/ui/home/TsParticles/TsParticles.tsx
@@ -2,21 +2,25 @@
 import { useCallback } from 'react';
 import Particles from 'react-particles';
 import type { Container, Engine } from 'tsparticles-engine';
-import { loadFull } from 'tsparticles'; // if you are going to use `loadFull`, install the "tsparticles" package too.
 import { loadSlim } from 'tsparticles-slim';
 
 const TsParticles = () => {
-  const particlesInit = useCallback(async (engine) => {
+  const particlesInit = useCallback(async (engine: Engine) => {
     console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
+    //await loadFull(engine);
+    await loadSlim(engine);
   }, []);
 
-  const particlesLoaded = useCallback(async (container) => {
-    await console.log(container);
-  }, []);
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    [],
+  );
   return (
     <div id="particle-background">
       <Particles
@@ -84,7 +88,7 @@ const TsParticles = () => {
               value: 0.5,
             },
             shape: {
-              type: 'circle',
+              type: 'triangle',
             },
             size: {
               value: { min: 1, max: 5 },

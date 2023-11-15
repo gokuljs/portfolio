3784b6753f7e9520e20c8f1d36ff67a11e24c360
gokuljs <gokuljs029@gmail.com>
2023-11-15 20:10:30 +0530

    experience section

--- src/app/ui/home/Experience/Experience.tsx
+++ src/app/ui/home/Experience/Experience.tsx
@@ -1,7 +1,111 @@
 import React from 'react';
 import styles from '@styles/Experience.module.scss';
+import clsx from 'clsx';
 const Experience = () => {
-  return <div className={styles.Experience}>sds</div>;
+  return (
+    <div className={styles.Experience}>
+      <h1 className={styles.heading}>Professional Experience</h1>
+      <div className={styles.timeline}>
+        <div className={clsx(styles.container, styles.leftContainer)}>
+          <div className={styles.textBox}>
+            <h2>Aerotime, Y Combinator (W21) | Founding Engineer</h2>
+            <small>Nov 2022 – Nov 2023 | San Francisco, California</small>
+            <ul>
+              <li>
+                Led the development of Aerotime's frontend infrastructure using
+                React, Vite, Next.js, Redux, and React Query, ensuring
+                scalability and efficiency.
+              </li>
+              <li>
+                Engaged directly with customers through discovery calls and
+                feedback sessions, using insights to drive user-centric product
+                development.
+              </li>
+              <li>
+                Communicated critical user feedback to company founders, playing
+                a key role in strategic decision-making and problem-solving
+                processes.
+              </li>
+              <li>
+                Deployed and managed open-source large language models like
+                LLaMA and Stable Diffusion, enhancing the company's AI
+                capabilities.
+              </li>
+              <li>
+                Employed advanced techniques from the Hugging Face library,
+                including DreamBooth and Textual Inversion, to optimize AI model
+                performance.
+              </li>
+              <li>
+                Maintained a focus on continuous technological improvement and
+                innovation, contributing to Aerotime's competitive edge in the
+                market.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={clsx(styles.container, styles.rightContainer)}>
+          <div className={styles.textBox}>
+            <h2>Tifin Fintech (Previously 1stMain)</h2>
+            <small>Mar 2022 – Nov 2022 | Bangalore, India</small>
+            <ul>
+              <li>
+                Spearheaded frontend development for TIFIN Wealth using Next.js
+                and React.js, increasing productivity by 20% through strategic
+                scaling and responsive design initiatives.
+              </li>
+              <li>
+                Streamlined application state management by implementing Redux
+                Toolkit, significantly enhancing workflow efficiency and
+                contributing to a 15% increase in customer satisfaction through
+                rigorous bug fixes.
+              </li>
+              <li>
+                Fostered a 50% improvement in development turnaround by creating
+                reusable React components and integrating APIs with GraphQL and
+                React Query for optimal frontend functionality.
+              </li>
+              <li>
+                Modernized the platform by migrating to updated codebases and
+                translating complex wireframes into user-centric websites,
+                maintaining full adherence to project timelines.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={clsx(styles.container, styles.leftContainer)}>
+          <div className={styles.textBox}>
+            <h2>1stMain</h2>
+            <small>Nov 2021 – Feb 2022 | Bangalore, India</small>
+            <ul>
+              <li>
+                Leveraged in-depth React expertise to enhance lifecycle methods,
+                boosting development efficiency by 40% and ensuring 100%
+                compliance with project delivery schedules.
+              </li>
+              <li>
+                Orchestrated the adaptation of over 50 client websites to meet
+                evolving industry standards, facilitating clear communication
+                among teams, clients, and stakeholders.
+              </li>
+              <li>
+                Pioneered the integration of content management systems (CMS)
+                like Strapi and Sanity, and executed seamless migrations between
+                GraphQL and REST API, optimizing content flow and website
+                interactivity.
+              </li>
+              <li>
+                Collaborated with UI/UX teams to develop and deploy responsive,
+                user-centric web applications using React.js, Gatsby.js, and
+                Next.js, culminating in highly engaging interfaces that align
+                with strategic visions.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;

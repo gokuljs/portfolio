8b757810173a3ad9fb7b3606d0b62385f911be4d
gokuljs <gokuljs029@gmail.com>
2023-11-15 19:17:04 +0530

    experence-section intialiazaation

---
+++ src/app/ui/home/Experience/Experience.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from '@styles/Experience.module.scss';
+const Experience = () => {
+  return <div className={styles.Experience}>sds</div>;
+};
+
+export default Experience;

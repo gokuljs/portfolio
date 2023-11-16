8f0127b33154de45f453d8f806641694650f90fb
gokuljs <gokuljs029@gmail.com>
2023-11-16 20:22:06 +0530

    navabr update

--- src/app/ui/home/Navbar/Navbar.tsx
+++ src/app/ui/home/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styles from '@styles/navar.module.scss';
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className={styles.Navbar}>
+      <div className={styles.logo}></div>
+      <div className={styles.items}>sdsdsd</div>
+    </div>
+  );
 };
 
 export default Navbar;

38d75a91d14383c58ca19ac1e71ed53cbf5e48ee
gokuljs <gokuljs029@gmail.com>
2023-11-16 21:02:10 +0530

    cv button

--- src/app/ui/home/Navbar/Navbar.tsx
+++ src/app/ui/home/Navbar/Navbar.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from '@styles/navar.module.scss';
+import { PinBottomIcon } from '@radix-ui/react-icons';
 
 const Navbar = () => {
   return (
     <div className={styles.Navbar}>
       <div className={styles.logo}></div>
-      <div className={styles.items}>sdsdsd</div>
+      <div className={styles.items}>
+        <div className={styles.Resume}>
+          <span>
+            Download CV <PinBottomIcon className={styles.icon} />
+          </span>
+        </div>
+      </div>
     </div>
   );
 };

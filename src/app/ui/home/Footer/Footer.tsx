40adab654a60a8962edaa676a80a7292def570a8
gokuljs <gokuljs029@gmail.com>
2023-11-16 13:21:00 +0530

    footer-section

--- src/app/ui/home/Footer/Footer.tsx
+++ src/app/ui/home/Footer/Footer.tsx
@@ -25,13 +25,22 @@ const Footer = () => {
             <Gmail className={styles.svgIcon} />
           </Link>
         </div>
-        <div className={styles.copyright}></div>
+        <div className={styles.copyright}>
+          © 2023 Gokuljs. All rights reserved
+        </div>
       </div>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <linearGradient id="myGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+          <stop offset="0%" style={{ stopColor: '#4A00E0', stopOpacity: 1 }} />
+          <stop
+            offset="100%"
+            style={{ stopColor: '#8E2DE2', stopOpacity: 1 }}
+          />
+        </linearGradient>
         <path
-          fill="#7e7ef1"
+          fill="url(#myGradient)"
           fill-opacity="1"
-          d="M0,64L40,101.3C80,139,160,213,240,240C320,267,400,245,480,202.7C560,160,640,96,720,74.7C800,53,880,75,960,106.7C1040,139,1120,181,1200,208C1280,235,1360,245,1400,250.7L1440,256L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+          d="M0,64L1440,192L1440,320L0,320Z"
         ></path>
       </svg>
     </div>

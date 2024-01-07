563a7685983cd6cd003ef5683d83007874f2a525
gokuljs <gokuljs029@gmail.com>
2024-01-07 16:35:29 +0530

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,9 @@ import Navbar from './ui/home/Navbar/Navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Gokul JS',
-  description: 'Ex-Founding Engineer at AeroTime, YC W21',
+  title: 'Gokul JS - Tech Innovator & Engineer',
+  description:
+    'Discover the portfolio of Gokul, an innovative tech professional with a track record of success as a Founding Engineer at AeroTime, Y Combinator W21. Explore my contributions to cutting-edge technology solutions, my skill set in modern web development, and the projects that showcase my journey in the tech industry.',
 };
 
 export default function RootLayout({

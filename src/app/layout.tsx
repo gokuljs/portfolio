0d8023f8b1cec407eff4726d71cf1b32533ed90b
gokuljs <gokuljs029@gmail.com>
2024-01-07 18:28:16 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from './ui/home/Navbar/Navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Gokul JS - Ex-Yc alumn w21',
+  title: 'Gokul JS - Ex-YC Alumnus W21',
   description:
     'Discover the portfolio of Gokul, an innovative tech professional with a track record of success as a Founding Engineer at AeroTime, Y Combinator W21. Explore my contributions to cutting-edge technology solutions, my skill set in modern web development, and the projects that showcase my journey in the tech industry.',
   keywords: [

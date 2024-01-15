03c510201fa68d8355a7f4858a2bc84374ee20b3
gokuljs <gokuljs029@gmail.com>
2024-01-15 19:06:19 +0530

    projects

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '@styles/projects.module.scss';
+
+const Page = () => {
+  return <div className={styles.projects}>Projects</div>;
+};
+
+export default Page;

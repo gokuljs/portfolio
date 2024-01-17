db19d5f88e57e602643ce5ae7aaf5aa3a6eda810
gokuljs <gokuljs029@gmail.com>
2024-01-17 09:50:11 +0530

    card

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import styles from '@styles/projects.module.scss';
 
 const Page = () => {
-  return <div className={styles.projects}>Projects</div>;
+  return (
+    <div className={styles.projects}>
+      <div className={styles.projectSection}>
+        <div className={styles.card}>card</div>
+        <div className={styles.card}>card</div>
+        <div className={styles.card}>card</div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;

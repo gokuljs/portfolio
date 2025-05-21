0ae3a9ef83523198bd8e588a046ecbd5042f5d20
gokuljs <gokuljs029@gmail.com>
2025-05-21 05:52:25 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import styles from '@styles/page.module.scss';
 import Hero from './ui/home/Hero/hero';
 import GithubGraph from './ui/home/GithubGraph/GithubGraph';
 import Skill from './ui/home/Skills/Skill';
-import TsParticles from './ui/home/TsParticles/TsParticles';
 import Experience from './ui/home/Experience/Experience';
 import Footer from './ui/home/Footer/Footer';
 

a5e406f96bc164fb9dced963f6fe629f7ca6c00f
gokuljs <gokuljs029@gmail.com>
2025-05-23 09:15:59 +0530

    fix

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,34 +9,36 @@ export default function ProjectsPage() {
     <div className="min-h-screen w-full bg-black">
       <Navbar />
       <div className="flex justify-center items-center">
-        <div className="container max-w-full min-h-screen px-4 !py-18">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-24 mt-24">
+        <div className="container max-w-7xl min-h-screen px-4 py-18">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-12 lg:gap-24 mt-24 p-4">
             {/* Project 1 with 3D Pin */}
-            <div className="h-96 w-full flex items-center justify-center">
+            <div className="w-full max-w-sm mx-auto">
               <PinContainer
                 title="Mailyx"
                 href="https://getmailyx.com/"
-                containerClassName="w-full h-full flex items-center justify-center"
+                containerClassName="w-full h-auto"
               >
-                <div className="relative  overflow-hidden  bg-gradient-to-r from-gray-900 to-black  group w-96">
+                <div className="relative overflow-hidden bg-gradient-to-r from-gray-900 to-black group w-80 h-fit rounded-2xl">
                   <div className="absolute inset-0 bg-gradient-to-r from-blue-500/20 to-cyan-400/10 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
                   <div
                     className="absolute inset-0 rounded-xl opacity-0 group-hover:opacity-100 transition-opacity"
                     style={{ boxShadow: '0 0 15px rgba(245, 245, 245, 0.4)' }}
                   ></div>
-                  <div className="relative h-full !min-h-[400px] flex flex-col  bg-gradient-to-b from-gray-900 to-black overflow-hidden p-6">
-                    <div className="!h-[15.5rem] w-full bg-center bg-no-repeat bg-cover bg-[url('/Mailyx.png')]"></div>
-                    <div className="!h-[30%] !p-4 flex flex-col justify-between gap-2">
-                      <h3 className="text-xl font-bold text-white mb-2">
-                        Mailyx
-                      </h3>
-                      <p className="text-gray-400 text-sm mb-4">
-                        Minimalist AI email client with smart inbox, search, and
-                        navigation.
-                      </p>
-                      <div className="flex space-x-2 mt-4">
-                        <span className="!px-2 !py-1 flex gap-2 items-center justify-center bg-yellow-900/40 text-yellow-300 text-xs rounded-full border border-yellow-700/50 flex items-center ">
-                          <span className="w-1.5 h-1.5 bg-yellow-400 rounded-full mr-1 animate-pulse"></span>
+                  <div className="relative h-full flex flex-col bg-gradient-to-b from-gray-900 to-black overflow-hidden p-6">
+                    <div className="h-50 w-full bg-center bg-no-repeat bg-cover bg-[url('/Mailyx.png')] rounded-lg mb-4"></div>
+                    <div className="flex-1 flex flex-col justify-between !px-4 !py-2 !pb-4 gap-4">
+                      <div>
+                        <h3 className="text-xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-white via-stone-200/10 to-transparent mb-2">
+                          Mailyx
+                        </h3>
+                        <p className="text-stone-400 text-sm mb-4 line-clamp-3">
+                          Minimalist AI email client with smart inbox, search,
+                          and navigation.
+                        </p>
+                      </div>
+                      <div className="flex space-x-2">
+                        <span className="!px-3 !py-1 flex gap-2 items-center bg-yellow-900/40 text-yellow-300 text-xs rounded-full border border-yellow-700/50">
+                          <span className="w-1.5 h-1.5 bg-yellow-400 rounded-full animate-pulse"></span>
                           Progress
                         </span>
                       </div>

b8328c23faeec1f244dce25c61a954eb5fdc3bc4
gokuljs <gokuljs029@gmail.com>
2025-06-01 04:46:25 +0530

    fix

--- src/app/ui/home/Hero/hero.tsx
+++ src/app/ui/home/Hero/hero.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 import styles from '@styles/hero.module.scss';
 import Navbar from '../../components/Navbar';
 import SocialDock from '../../components/SocialDock/SocialDock';
+import { Spotlight } from '@/components/ui/spotlight-new';
 
 const Hero: React.FC = () => {
   return (
     <div className={styles.hero}>
       <Navbar />
+      <div className="hidden md:block">
+        <Spotlight
+          gradientFirst="radial-gradient(68.54% 68.72% at 55.02% 31.46%, hsla(210, 100%, 85%, .12) 0, hsla(210, 100%, 55%, .04) 50%, hsla(210, 100%, 45%, 0) 80%)"
+          gradientSecond="radial-gradient(50% 50% at 50% 50%, hsla(210, 100%, 85%, .08) 0, hsla(210, 100%, 55%, .03) 80%, transparent 100%)"
+          gradientThird="radial-gradient(50% 50% at 50% 50%, hsla(210, 100%, 85%, .06) 0, hsla(210, 100%, 45%, .02) 80%, transparent 100%)"
+          translateY={-350}
+          width={560}
+          height={1380}
+          smallWidth={240}
+          duration={6}
+          xOffset={140}
+        />
+      </div>
+
       <div></div>
       <div className={styles.content}>
         <div className={styles.mainTextContainer}>

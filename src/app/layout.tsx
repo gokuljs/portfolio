8c494ec5f05d85edc60d231feadd0ff8f9e02082
gokuljs <gokuljs029@gmail.com>
2025-06-08 09:23:30 +0530

    Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@styles/globals.scss';
 import VisitTracker from '@/components/VisitTracker';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -110,6 +111,7 @@ export default function RootLayout({
         <VisitTracker />
         {/* <Navbar /> */}
         {children}
+        <Analytics />
       </body>
     </html>
   );

e9ff847950e3384fbfa33f8ace6bebcce6d1edd4
Gokul Js <gokuljs029@gmail.com>
2026-03-05 00:07:49 +0530

    Update change frequency for blogs in sitemap to weekly

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,7 +14,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${BASE_URL}/blogs`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'weekly',
       priority: 0.9,
     },
     {
